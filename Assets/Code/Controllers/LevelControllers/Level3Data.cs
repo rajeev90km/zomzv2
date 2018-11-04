c4822112b640df0a91e8523b80e15cece57b9e2e
Rajeev Mukundan <rajeev90km@gmail.com>
2018-11-04 18:20:40 -0500

    Level 3 Base setup

---
+++ Assets/Code/Controllers/LevelControllers/Level3Data.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Zomz/Data/Level3Data", fileName = "Level3Data")]
+public class Level3Data : LevelData {
+    
+
+}
